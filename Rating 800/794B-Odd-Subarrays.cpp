d9b1d25ccf21a8396f76b49ab665c261fba73b3b
Mneach <mneachdev@gmail.com>
2023-05-23 01:29:27 +0700

    Added problem solution for 794B-Odd-Subarrays.cpp

---
+++ Rating 800/794B-Odd-Subarrays.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+
+#define checkAnswer(x)  cout << "Answer : " << x << endl;
+#define all(x) x.begin(), x.end()
+
+ull maxULL(ull a , ull b){
+    return (a > b) ? a : b;
+}
+
+ull minULL(ull a , ull b){
+    return (a > b) ? b : a;
+}
+
+ll maxLL(ll a , ll b){
+    return (a > b) ? a : b;
+}
+
+ll minLL(ll a , ll b){
+    return (a > b) ? b : a;
+}
+
+int main()
+{
+
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+
+        int answer = 0;
+        vi arr(n);
+
+        for (int j = 0; j < n; j++) cin >> arr[j];
+
+        for (int j = 0; j < n - 1; j++)
+        {
+            if(arr[j] > arr[j + 1]){
+                answer++;
+                j++;
+            }
+        }
+        
+        cout << answer << endl;
+    }
+    
+
+    return 0;
+}
