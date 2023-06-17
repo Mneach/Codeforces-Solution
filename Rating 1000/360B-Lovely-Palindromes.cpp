2967e2858563f4fb621cdaa815baab7282268d79
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-06-17 14:32:31 +0700

    Added problem solution for 360B-Lovely-Palindromes.cpp

---
+++ Rating 1000/360B-Lovely-Palindromes.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+typedef vector<pair<int,int>> vpi;
+
+#define checkAnswer(x)  cout << "Answer : " << x << endl;
+#define all(x) x.begin(), x.end()
+
+ull maxULL(ull a , ull b){
+    return (a > b) ? a : b;
+}
+
+ull minULL(ull a , ull b){
+    return (a > b) ? b : a;
+}
+
+ll maxLL(ll a , ll b){
+    return (a > b) ? a : b;
+}
+
+ll minLL(ll a , ll b){
+    return (a > b) ? b : a;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    for(int i = 0; i < s.length(); i++){
+        cout << s[i];
+    }
+
+    for(int i = s.length() - 1; i >= 0; i--){
+        cout << s[i];
+    }
+
+    cout << endl;
+}
