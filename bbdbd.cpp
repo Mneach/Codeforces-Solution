22f1bc50a0ac4638ed6e3e4054944d0a4e425957
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-08-06 18:13:19 +0700

    Add files via upload

---
+++ bbdbd.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+
+int n;
+
+cin >> n;
+
+vector<int> arr(n);
+
+for(int i = 0; i < n; i++){
+cin >> arr[i];
+}
+
+for(int i = 1; i < n - 1; i++){
+
+if(arr[i - 1] < arr[i] && arr[i] > arr[i + 1]){
+ cout << "YES" << endl;
+ cout << i << " " << i + 1<< " " << i + 2 << endl;
+
+return;
+
+}
+}
+cout << "NO" << endl;
+
+}
+
+int main(){
+
+
+int t;
+cin >> t;
+
+while(t--){
+solve();
+
+}
+
+return 0;
+}
