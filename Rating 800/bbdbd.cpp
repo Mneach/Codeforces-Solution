e922af941e6bb6ebd9f8aa2e2a85a6c3c1b7001b
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-08-07 15:10:05 +0700

    Add files via upload

---
+++ Rating 800/bbdbd.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+
+long long int n;
+
+cin >> n;
+
+cout << (n + 1) / 2 << endl;
+}
+
+int main(){
+
+
+int t;
+cin >> t;
+
+while(t--){
+solve();
+
+}
+
+return 0;
+}
